c5ffd0534ea9f10c7ea3809aed5006dfb1e4f523
CrazyMax <crazy-max@users.noreply.github.com>
2017-09-16 14:37:04 +0200

    Change logger

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/op/go-logging"
+	"github.com/google/logger"
 )
 
 const (
@@ -19,70 +19,64 @@ const (
 	APP_PROCESS     = "Brave.exe"
 )
 
-var (
-	log       = logging.MustGetLogger(NAME)
-	logFormat = logging.MustStringFormatter(`%{time:2006-01-02 15:04:05} %{level:.4s} - %{message}`)
-)
-
 func main() {
 	// Current path
 	currentPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
-		log.Error("Current path:", err)
+		logger.Error("Current path:", err)
 	}
 
 	// Log file
 	logfile, err := os.OpenFile(pathJoin(currentPath, NAME+".log"), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
-		log.Error("Log file:", err)
+		logger.Error("Log file:", err)
 	}
+	defer logfile.Close()
 
 	// Init logger
-	logBackendStdout := logging.NewBackendFormatter(logging.NewLogBackend(os.Stdout, "", 0), logFormat)
-	logBackendFile := logging.NewBackendFormatter(logging.NewLogBackend(logfile, "", 0), logFormat)
-	logging.SetBackend(logBackendStdout, logBackendFile)
-	log.Info("--------")
-	log.Info("Starting " + NAME + "...")
-	log.Info("Current path:", currentPath)
+	logger.Init(NAME, false, false, logfile)
+	logger.Info("--------")
+	logger.Infof("Starting %s...", NAME)
+	logger.Infof("Current path: %s", currentPath)
 
 	// Find app folder
-	log.Info("Lookup app folder in", currentPath)
-	var appPath = ""
+	logger.Infof("Lookup app folder in: %s", currentPath)
+	appPath := ""
 	rootFiles, _ := ioutil.ReadDir(currentPath)
 	for _, f := range rootFiles {
 		if strings.HasPrefix(f.Name(), "app-") && f.IsDir() {
-			log.Info("App folder found:", f.Name())
+			logger.Infof("App folder found: %s", f.Name())
 			appPath = pathJoin(currentPath, f.Name())
 			break
 		}
 	}
 	if _, err := os.Stat(appPath); err == nil {
-		log.Info("App path:", appPath)
+		logger.Infof("App path: %s", appPath)
 	} else {
-		log.Error("App path does not exist")
+		logger.Error("App path does not exist")
 	}
 
 	// Init vars
 	appExe := pathJoin(appPath, APP_PROCESS)
 	dataPath := pathJoin(currentPath, "data")
 	dataAppPath := pathJoin(dataPath, "AppData", "Roaming", APP_DATA_FOLDER)
-	log.Info("App executable:", appExe)
-	log.Info("Data path:", dataPath)
+	logger.Infof("App executable: %s", appExe)
+	logger.Infof("Data path: %s", dataPath)
 
 	// Create data folder
-	log.Info("Create data folder...", dataAppPath)
+	logger.Infof("Create data folder %s...", dataAppPath)
 	err = os.MkdirAll(dataAppPath, 777)
 	if err != nil {
-		log.Error("Create data folder:", err)
+		logger.Error("Create data folder:", err)
 	}
 
 	// Override USERPROFILE env var
 	if err := os.Setenv("USERPROFILE", dataPath); err != nil {
-		log.Error(err)
+		logger.Error("Cannot set USERPROFILE env var:", err)
 	}
 
 	// Launch
-	log.Infof("Launch %s...", APP_NAME)
+	logger.Infof("Launch %s...", APP_NAME)
 	execApp := exec.Command(appExe)
 	execApp.Dir = appPath
 
@@ -91,7 +85,7 @@ func main() {
 	execApp.Stderr = logfile
 
 	if err := execApp.Start(); err != nil {
-		log.Error("Cmd Start:", err)
+		logger.Error("Cmd Start:", err)
 	}
 
 	execApp.Wait()
