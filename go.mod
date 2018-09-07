2ee4253be18040bfeceb09b2a24167dadf1b0c14
CrazyMax <crazy-max@users.noreply.github.com>
2018-09-07 18:22:43 +0200

    Upgrade to Go 1.11 Use go mod instead of dep

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/portapps/brave-portable
+
+require github.com/portapps/portapps v0.0.0-20180907012234-e28025ce2b66
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-//go:generate go get -v github.com/josephspurrier/goversioninfo/...
+//go:generate go install -v github.com/josephspurrier/goversioninfo/cmd/goversioninfo
 //go:generate goversioninfo -icon=res/papp.ico
 package main
 
