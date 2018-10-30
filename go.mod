622c8ba9f8b86fdb62b5006d840c0957d85592a9
CrazyMax <crazy-max@users.noreply.github.com>
2018-10-30 23:16:11 +0100

    Test 0.55 (Issue #4)

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module github.com/portapps/brave-portable
 
-require github.com/portapps/portapps v0.0.0-20181003014304-a794d990c87f
+require (
+	github.com/kevinburke/go-bindata v3.11.0+incompatible
+	github.com/portapps/portapps v0.0.0-20181024181948-e16569932064
+)
--- main.go
+++ main.go
@@ -1,10 +1,16 @@
+//go:generate go install -v github.com/kevinburke/go-bindata/go-bindata
+//go:generate go-bindata -prefix res/ -pkg assets -o assets/assets.go res/Brave.lnk
 //go:generate go install -v github.com/josephspurrier/goversioninfo/cmd/goversioninfo
 //go:generate goversioninfo -icon=res/papp.ico
 package main
 
 import (
+	"io/ioutil"
 	"os"
+	"path"
 
+	_ "github.com/kevinburke/go-bindata"
+	"github.com/portapps/brave-portable/assets"
 	. "github.com/portapps/portapps"
 )
 
@@ -17,12 +23,40 @@ func init() {
 func main() {
 	Papp.AppPath = AppPathJoin("app")
 	Papp.DataPath = CreateFolder(AppPathJoin("data"))
+	Papp.Process = PathJoin(Papp.AppPath, "brave.exe")
+	Papp.Args = []string{
+		"--user-data-dir=" + Papp.DataPath,
+		"--disable-brave-update",
+		"--no-default-browser-check",
+		"--allow-outdated-plugins",
+		"--disable-logging",
+		"--disable-breakpad",
+	}
+	Papp.WorkingDir = Papp.AppPath
 
-	electronBinPath := PathJoin(Papp.AppPath, FindElectronAppFolder("app-", Papp.AppPath))
+	// Copy default shortcut
+	shortcutPath := path.Join(os.Getenv("APPDATA"), "Microsoft", "Windows", "Start Menu", "Programs", "Brave Portable.lnk")
+	defaultShortcut, err := assets.Asset("Brave.lnk")
+	if err != nil {
+		Log.Error("Cannot load asset Brave.lnk:", err)
+	}
+	err = ioutil.WriteFile(shortcutPath, defaultShortcut, 0644)
+	if err != nil {
+		Log.Error("Cannot write default shortcut:", err)
+	}
 
-	Papp.Process = PathJoin(Papp.AppPath, "Brave.exe")
-	Papp.Args = nil
-	Papp.WorkingDir = electronBinPath
+	// Update default shortcut
+	err = CreateShortcut(WindowsShortcut{
+		ShortcutPath:     shortcutPath,
+		TargetPath:       Papp.Process,
+		Arguments:        WindowsShortcutProperty{Clear: true},
+		Description:      WindowsShortcutProperty{Value: "Brave Portable by Portapps"},
+		IconLocation:     WindowsShortcutProperty{Value: Papp.Process},
+		WorkingDirectory: WindowsShortcutProperty{Value: Papp.AppPath},
+	})
+	if err != nil {
+		Log.Error("Cannot create shortcut:", err)
+	}
 
 	Launch(os.Args[1:])
 }
