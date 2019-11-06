1a396bf1fbd84845a9adbf1c83959b165d534a24
CrazyMax <crazy-max@users.noreply.github.com>
2019-11-06 17:17:08 +0100

    Manifest app for better reliability

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 //go:generate go install -v github.com/kevinburke/go-bindata/go-bindata
 //go:generate go-bindata -prefix res/ -pkg assets -o assets/assets.go res/Brave.lnk
 //go:generate go install -v github.com/josephspurrier/goversioninfo/cmd/goversioninfo
-//go:generate goversioninfo -icon=res/papp.ico
+//go:generate goversioninfo -icon=res/papp.ico -manifest=res/papp.manifest
 package main
 
 import (
